0a985dff215c3b56580098d32bc8377a8f3aae95
Rimmert Zelle <rimmert.zelle@gmail.com>
2024-09-25 09:35:30 +0200

    Addes class 4 start folder

---
+++ class-4/start/src/lib/components/Client.svelte
@@ -0,0 +1,5 @@
+<script></script>
+
+<div>
+    <h2>Details</h2>
+</div>
---
+++ class-4/start/src/lib/components/TimeSlot.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let contents;
+</script>
+
+<div class="appointment" id={contents.id}>
+	{#if contents.appointment}
+		<ul class="bg-amber-100 flex flex-wrap justify-between rounded-lg">
+			<li class="mr-2 p-2 rounded-l-lg">{contents.starttime}</li>
+			<li class="mr-2 p-2">{contents.appointment.name}</li>
+			<li class="mr-2 p-2">{contents.appointment.breed}</li>
+		</ul>
+	{:else}
+		<ul class="flex flex-wrap justify-between bg-slate-100 rounded-lg">
+			<li class="bg-slate-100 mr-2 p-2 rounded-l-lg">{contents.starttime}</li>
+			<li class="bg-slate-100 mr-2 p-2">No appointment</li>
+		</ul>
+	{/if}
+</div>
